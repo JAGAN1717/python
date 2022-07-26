4f761aa60201c5023fa7b1210b01372d0128dcf1
J17 <102845567+Jagan1707@users.noreply.github.com>
2022-07-26 11:44:03 +0530

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,24 @@
+tuple = ("apple", "banana", "orange")
+print(tuple)
+
+
+tuple2 = ("apple", "banana", "orange", "apple", "orange")
+print(tuple2)
+
+tuple3 = tuple4(("apple", "banana", "orange"))
+print(len(tuple))
+
+tuple5 = ("apple",)
+print(type(tuple5))
+
+# str
+tuple6 = ("apple")
+print(type(tuple6))
+
+tuple7 = ("apple", "banana", "orange")
+tuple8 = (1, 5, 7, 9, 3)
+tuple9 = (True, False, False)
+
+print(tuple7)
+print(tuple8)
+print(tuple9)
