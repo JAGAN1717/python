b2dbf4143cd552be4f78c082a175c619412750bf
J17 <102845567+Jagan1707@users.noreply.github.com>
2022-07-26 11:42:27 +0530

    Create number.py

---
+++ number.py
@@ -0,0 +1,24 @@
+x = 2
+y = 346717253
+z = -987754
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 2.20
+y = 5.045
+z = -87.26
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 87e3
+y = 23E4
+z = -56.8e100
+
+print(type(x))
+print(type(y))
+print(type(z))
