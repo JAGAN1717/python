e429a03b106ac692fdf4dbd26e74d8f09d5f4833
J17 <102845567+Jagan1707@users.noreply.github.com>
2022-07-26 11:39:27 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,22 @@
+def function():
+  print("Hello")
+
+function()
+
+
+
+def function2(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+function2(child1 = "lion", child2 = "tiger", child3 = "cheeta")
+
+
+
+
+def function3(foods):
+  for x in foods:
+    print(x)
+
+fruits = ["apple", "banana", "orange"]
+
+function3(fruits)
