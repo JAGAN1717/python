0797dacf3a729b1a7aff8b3dab77c1b68fa87c78
J17 <102845567+Jagan1707@users.noreply.github.com>
2022-07-26 11:43:17 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,13 @@
+set = {"chennai", "andhra", "kerala"}
+print(set)
+
+print(type(set))
+
+
+sets = set(("chennai", "andhra", "kerala"))
+print(sets)
+
+
+set1 = {"abc", 24, True, 20, "male"}
+
+print(set1)
